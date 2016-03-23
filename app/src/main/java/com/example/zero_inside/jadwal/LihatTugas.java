30ab8610c0e73fd6282934a51a9ab77a4a6ba00b
Satrya Budi Pratama <zeroonetm@gmail.com>
2016-03-23 10:12:42 +0700

    Jadwal Kuliah IF39-08
    Build with love

---
+++ app/src/main/java/com/example/zero_inside/jadwal/LihatTugas.java
@@ -0,0 +1,13 @@
+package com.example.zero_inside.jadwal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LihatTugas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_tugas);
+    }
+}
